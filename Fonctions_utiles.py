f972031aee83bed85f786fa939e6ace7ede5e4be
Anaëlle ROBIN <anaelle.robin@cpe.fr>
2024-10-01 10:58:29 +0200

    améliorations

--- Fonctions_utiles.py
+++ Fonctions_utiles.py
@@ -19,3 +19,42 @@ def lire_txt(fichiertxt):
     with open(fichiertxt, 'r', encoding='utf-8') as fichier:
         liste = [ligne.strip() for ligne in fichier.readlines() if ligne.strip()] # Lire lignes et supprimer les espaces blancs
     return liste
+
+def afficher_mot(solution,lettre_trouvees):
+    """
+    Fonction qui réecrit l'affichage d'un mot en fonction des lettres trouvés
+    Variables :
+        affichage : str
+        lettre_trouvees : str
+        solution : str
+    Sortie :
+        affichage : str
+    """
+    affichage = ""
+    for index,lettre in enumerate(solution) :
+        if lettre in lettre_trouvees:
+            affichage += lettre + " "
+        elif index == 0:
+            affichage += solution[0] + " " 
+        else:
+            affichage += "_ "
+    return affichage
+
+def ask_rejouer():
+    """
+    Fonction qui demande si le joueur veux rejouer
+    Variables :
+        rejouer : str
+    Sortie :
+        True / False
+    """
+    rejouer = ""
+    while rejouer != ('oui' or 'non'):
+        rejouer = input("Voulez vous rejouer ? (oui/non): ").lower()
+        if rejouer == 'oui':
+            return True
+        if rejouer == 'non':
+            return False
+        else :
+            print("Veuillez répondre par 'oui' ou 'non'")
+        
--- Pendu.py
+++ Pendu.py
@@ -11,36 +11,37 @@ TODO:
 import random as r
 import Fonctions_utiles as f #NE PAS METTRE .py SINON NE MARCHE PAS
 
-Liste_de_mot = f.lire_txt('Mots.txt')
-solution = Liste_de_mot[r.randint(0,len(Liste_de_mot) -1)].lower()
-tentatives = 8
-affichage = solution[0] #extrait la premiere lettre
-lettre_trouvees = ""
-for lettre in range(len(solution)-1) :
-    affichage += " _"
-
+play = True
 print(">>> Bienvenu dans le jeu du PENDU ! <<<")
 
-while tentatives != 0 :
-    print(f"Mot à deviner :  {affichage}")
-    lettre_joueur = str(input("Veuillez rentrer votre lettre : ")).lower(   )
-    if lettre_joueur in solution :
-        print("-> Bien joué !")
-        lettre_trouvees += lettre_joueur 
-        affichage = ""
-        for index,lettre in enumerate(solution) :
-            if lettre in lettre_trouvees:
-                affichage += lettre + " "
-            elif index == 0:
-                affichage += solution[0] + " " 
-            else:
-                affichage += "_ "
-    else :
-        tentatives -= 1
-        print(f"-> Raté ! Il vous reste {tentatives} tentatives.")
-        if tentatives == 0:
-             print(f">>> Vous avez perdu ! Le mot était {solution}. <<<")
-    if "_" not in affichage :
-        print(f">>> Félicitations ! Vous avez deviné le mot : {solution} <<<")
-        break
+while play == True :
+    Liste_de_mot = f.lire_txt('Mots.txt')
+    solution = Liste_de_mot[r.randint(0,len(Liste_de_mot) -1)].lower()
+    tentatives = 8
+    affichage = solution[0] #extrait la premiere lettre
+    lettres_justes = ""
+    lettres_trouvees = ""
+    for lettre in range(len(solution)-1) :
+        affichage += " _"
+   
+    while tentatives != 0 :
+        print(f"Mot à deviner :  {affichage}")
+        lettre_joueur = str(input("Veuillez rentrer votre lettre : ")).lower()
+        if lettre_joueur in lettres_trouvees:
+            print(f"La lettre '{lettre_joueur}' à déjà été donnée. \nListe des lettres déjà essayées : {lettres_trouvees}")
+        else :
+            lettres_trouvees += lettre_joueur + " "
+            if lettre_joueur in solution :
+                print("-> Bien joué !")
+                lettres_justes += lettre_joueur 
+                affichage = f.afficher_mot(solution,lettres_justes)
+            else :
+                tentatives -= 1
+                print(f"-> Raté ! Il vous reste {tentatives} tentatives.")
+                if tentatives == 0:
+                    print(f">>> Vous avez perdu ! Le mot était '{solution}. <<<")
+        if "_" not in affichage :
+            print(f">>> Félicitations ! Vous avez deviné le mot : {solution} <<<")
+            break
+    play = f.ask_rejouer()
 print("*** FIN DU JEU ***")
